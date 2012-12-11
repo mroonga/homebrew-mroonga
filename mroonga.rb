724dd9a6d8d8e51e581503822713bec43f92f4cb
Kouhei Sutou <kou@clear-code.com>
2012-12-11 13:53:09 +0900

    2.08 -> 2.09

--- mroonga.rb
+++ mroonga.rb
@@ -4,8 +4,8 @@ require 'formula'
 
 class Mroonga < Formula
   homepage 'http://mroonga.github.com/'
-  url 'https://github.com/downloads/mroonga/mroonga/mroonga-2.08.tar.gz'
-  md5 'bb04d973569a1b17f811fc344c0feb95'
+  url 'https://github.com/downloads/mroonga/mroonga/mroonga-2.09.tar.gz'
+  md5 '775385ef67d4ac73da9acbecc467af18'
 
   if ARGV.include?("--use-homebrew-mysql")
     depends_on 'mysql'
