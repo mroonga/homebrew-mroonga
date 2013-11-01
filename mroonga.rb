f8f556aeab2f3f1439ec243ab2a516199a4decc6
HAYASHI Kentaro <hayashi@clear-code.com>
2013-11-01 18:50:01 +0900

    Install missing UDF (mroonga_escape)

--- mroonga.rb
+++ mroonga.rb
@@ -141,6 +141,7 @@ class Mroonga < Formula
       "CREATE FUNCTION last_insert_grn_id RETURNS INTEGER SONAME \"ha_mroonga.so\";",
       "CREATE FUNCTION mroonga_snippet RETURNS STRING SONAME \"ha_mroonga.so\";",
       "CREATE FUNCTION mroonga_command RETURNS STRING SONAME \"ha_mroonga.so\";",
+      "CREATE FUNCTION mroonga_escape RETURNS STRING SONAME \"ha_mroonga.so\";"
     ]
     sqls.join(" ")
   end
