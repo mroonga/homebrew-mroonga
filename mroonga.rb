054c9aa442b702e337bbb3d2a3832a5523d2ce37
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-08-02 17:58:15 +0900

    Mroonga 5.05 has been released!!

--- mroonga.rb
+++ mroonga.rb
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 class Mroonga < Formula
   homepage "http://mroonga.org/"
-  url "http://packages.groonga.org/source/mroonga/mroonga-5.04.tar.gz"
-  sha256 "cc21ffcbc991e80c4fac1870fbfffcb9d3556ef63138834dde4e42472274dbef"
+  url "http://packages.groonga.org/source/mroonga/mroonga-5.05.tar.gz"
+  sha256 "03a8e8fea4854e769c1d5289baedab21d6ecc0375791af49033d9552aa9d3599"
 
   depends_on "pkg-config" => :build
   depends_on "groonga-normalizer-mysql"
